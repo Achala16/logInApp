ba2495ee1e4a8ae0d080877567567d61534e33fb
achal <achal@_DHANANJAYA_>
2023-07-16 19:42:56 +0530

    Add project files.

---
+++ logInApp/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using logInApp.Model;
+using logInApp.View;
+using logInApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace logInApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Student> STUDENTS { get; set; } = GetStudents();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            STUDENTS = GetStudents();
+        }
+
+
+        private void Close_App_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void open_add_Click(object sender, RoutedEventArgs e)
+        {
+            AddStudent Add_Student = new AddStudent();
+            Add_Student.Show();
+
+        }
+
+        private void open_edit_Click(object sender, RoutedEventArgs e)
+        {
+            EditStudent editStudent = new EditStudent();
+            editStudent.Show();
+        }
+
+        private void open_delete_Click(object sender, RoutedEventArgs e)
+        {
+            DeleteStudent deleteStudent = new DeleteStudent();
+            deleteStudent.Show();
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        public static List<Student> GetStudents()
+        {
+            var list = new List<Student>();
+            using (var ctx = new DataContext())
+            {
+                foreach (var p in ctx.Students)
+                {
+                    list.Add(p);
+                }
+            }
+            return list;
+        }
+
+        private void StudentGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ logInApp/View Model/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using logInApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace logInApp.ViewModel
+{
+    public class DataContext : DbContext
+    {
+
+        //setting the data base tables
+        public DbSet<Student> Students { get; set; }
+
+        public string dbPath = @"C:\Users\achal\source\repos\logInApp\logInApp\DtB.db";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite($"Data Source={dbPath}");
+    }
+}
